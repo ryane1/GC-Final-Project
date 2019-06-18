85260ce3e9401768f4123fcda4cdd231c686f976
Heather Wenzel <hcwenzel@gmail.com>
2019-06-18 10:54:19 -0400

    working ngIf method

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -7,14 +7,35 @@ import { ApiService } from "../api.service";
   styleUrls: ["./story.component.css"]
 })
 export class StoryComponent implements OnInit {
+  showAngerDisgust: boolean = false;
+  showSadnessFear: boolean = false;
+  showJoySurprise: boolean = false;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {}
 
   analyzeUserData(form) {
-    this.apiService.getEmotionAnalysis(form.value.textInput).subscribe(response => {
-      console.log(form.value.textInput);
-      console.log(response);
-    });
+    this.apiService
+      .getEmotionAnalysis(form.value.textInput)
+      .subscribe(response => {
+        console.log(form.value.textInput);
+        console.log(response);
+        if (response.emotions_detected[0] == "joy") {
+          this.showJoySurprise = true;
+        } else if (response.emotions_detected[0] == "surpise") {
+          this.showJoySurprise = true;
+        } else if (response.emotions_detected[0] == "anger") {
+          this.showAngerDisgust = true;
+        } else if (response.emotions_detected[0] == "disgust") {
+          this.showAngerDisgust = true;
+        } else if (response.emotions_detected[0] == "sadness") {
+          this.showSadnessFear = true;
+        } else if (response.emotions_detected[0] == "fear") {
+          this.showSadnessFear = true;
+        } else {
+          this.showJoySurprise = true;
+        }
+      });
   }
 }
