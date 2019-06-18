3e819b09c86be19230e1f60fb243e54d39f996eb
nikol899 <nikol.899@gmail.com>
2019-06-18 11:58:51 -0400

    desktop-view done

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -7,9 +7,9 @@ import { ApiService } from "../api.service";
   styleUrls: ["./story.component.css"]
 })
 export class StoryComponent implements OnInit {
-  showAngerDisgust: boolean = false;
-  showSadnessFear: boolean = false;
-  showJoySurprise: boolean = false;
+  showAngerDisgust: boolean = true;
+  showSadnessFear: boolean = true;
+  showJoySurprise: boolean = true;
 
   constructor(private apiService: ApiService) {}
 
@@ -21,21 +21,22 @@ export class StoryComponent implements OnInit {
       .subscribe(response => {
         console.log(form.value.textInput);
         console.log(response);
-        if (response.emotions_detected[0] == "joy") {
-          this.showJoySurprise = true;
-        } else if (response.emotions_detected[0] == "surpise") {
-          this.showJoySurprise = true;
-        } else if (response.emotions_detected[0] == "anger") {
-          this.showAngerDisgust = true;
-        } else if (response.emotions_detected[0] == "disgust") {
-          this.showAngerDisgust = true;
-        } else if (response.emotions_detected[0] == "sadness") {
-          this.showSadnessFear = true;
-        } else if (response.emotions_detected[0] == "fear") {
-          this.showSadnessFear = true;
-        } else {
-          this.showJoySurprise = true;
-        }
-      });
+      
+  //       if (response.emotions_detected[0] == "joy") {
+  //         this.showJoySurprise = true;
+  //       } else if (response.emotions_detected[0] == "surpise") {
+  //         this.showJoySurprise = true;
+  //       } else if (response.emotions_detected[0] == "anger") {
+  //         this.showAngerDisgust = true;
+  //       } else if (response.emotions_detected[0] == "disgust") {
+  //         this.showAngerDisgust = true;
+  //       } else if (response.emotions_detected[0] == "sadness") {
+  //         this.showSadnessFear = true;
+  //       } else if (response.emotions_detected[0] == "fear") {
+  //         this.showSadnessFear = true;
+  //       } else {
+  //         this.showJoySurprise = true;
+  //       }
+  });
   }
 }
