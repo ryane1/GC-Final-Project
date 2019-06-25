cdecf364e40344c51c94378ec44be96769599926
Ryan E <ryane@yahoo.com>
2019-06-25 10:45:47 -0400

    added sadness audio and new updates

--- src/app/story/sadness-fear/sadness-fear.component.ts
+++ src/app/story/sadness-fear/sadness-fear.component.ts
@@ -12,4 +12,35 @@ export class SadnessFearComponent implements OnInit {
     el.scrollIntoView();
   }
   ngOnInit() {}
+
+playAudio(){
+  let audio = new Audio();
+  audio.src = "../../../assets/sounds/Grasshopper.mp3";
+  audio.load();
+  audio.play();
+}
+playAudio2(){
+  let audio = new Audio();
+  audio.src="../../../assets/sounds/Thunderstorm.mp3";
+  audio.load();
+  audio.play();
+}
+playAudio3(){
+  let audio = new Audio();
+  audio.src="../../../assets/sounds/Bicycle Bell Ringing.mp3";
+  audio.load();
+  audio.play();
+}
+playAudio4(){
+  let audio = new Audio();
+  audio.src="../../../assets/sounds/Squish.mp3";
+  audio.load();
+  audio.play();
+}
+playAudio5(){
+  let audio = new Audio();
+  audio.src="../../../assets/sounds/Door Close.mp3";
+  audio.load();
+  audio.play();
+}
 }
